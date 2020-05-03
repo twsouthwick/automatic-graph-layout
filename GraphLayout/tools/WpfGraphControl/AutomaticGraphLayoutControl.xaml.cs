637327db77a37db48398f2202ebfb104d07b65fb
Taylor Southwick <twsouthwick@outlook.com>
2020-05-03 12:08:59 -0700

    Clear children of dockpanel and add Viewer dependencyProperty

--- GraphLayout/tools/WpfGraphControl/AutomaticGraphLayoutControl.xaml.cs
+++ GraphLayout/tools/WpfGraphControl/AutomaticGraphLayoutControl.xaml.cs
@@ -3,26 +3,41 @@ using Microsoft.Msagl.Drawing;
 
 namespace Microsoft.Msagl.WpfGraphControl {
     public partial class AutomaticGraphLayoutControl {
+        private static readonly DependencyPropertyKey ViewerPropertyKey =
+                DependencyProperty.RegisterReadOnly(nameof(Viewer), typeof(IViewer), typeof(AutomaticGraphLayoutControl), new PropertyMetadata());
+
+        public static readonly DependencyProperty ViewerProperty = ViewerPropertyKey.DependencyProperty;
+
+        public static readonly DependencyProperty GraphProperty =
+                DependencyProperty.Register(nameof(Graph), typeof(Graph), typeof(AutomaticGraphLayoutControl), new PropertyMetadata(default(Graph),
+                    (d, e) => ((AutomaticGraphLayoutControl)d)?.SetGraph()));
+
         public AutomaticGraphLayoutControl() {
             InitializeComponent();
-            Loaded += (s, e) => SetGraph();
         }
+
         public Graph Graph {
             get => (Graph)GetValue(GraphProperty);
             set => SetValue(GraphProperty, value);
         }
-        public static readonly DependencyProperty GraphProperty =
-            DependencyProperty.Register("Graph", typeof(Graph), typeof(AutomaticGraphLayoutControl), new PropertyMetadata(default(Graph),
-                (d,e)=> ((AutomaticGraphLayoutControl)d)?.SetGraph()));
-     
+
+        public IViewer Viewer {
+            get => (IViewer)GetValue(ViewerProperty);
+            private set => SetValue(ViewerPropertyKey, value);
+        }
+
         private void SetGraph() {
-            if (Graph == null) {
-                dockPanel.Children.Clear();
+            dockPanel.Children.Clear();
+
+            if (Graph is null) {
                 return;
             }
+
             var graphViewer = new GraphViewer();
             graphViewer.BindToPanel(dockPanel);
             graphViewer.Graph = Graph;
+
+            Viewer = graphViewer;
         }
     }
 }
